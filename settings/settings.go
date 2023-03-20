183dee039e74288ca8cb24043f421834f827d348
cnych <ych_1024@163.com>
2023-03-20 18:24:28 +0800

    fix config path

--- settings/settings.go
+++ settings/settings.go
@@ -11,7 +11,7 @@ import (
 func Init() (err error) {
 	viper.SetConfigName("config") // 指定配置文件名称（不要带后缀）
 	viper.SetConfigType("yaml")   // 指定配置文件类型
-	viper.AddConfigPath(".")      // 指定查找配置文件的路径（相对路径）
+	viper.AddConfigPath("./conf")      // 指定查找配置文件的路径（相对路径）
 	// 读取配置信息
 	if err = viper.ReadInConfig(); err != nil {
 		return
